089907c0c4166d2167ce336e8f071f114772cb30
Isaiah Davis <isaiah.davisx@gmail.com>
2021-01-08 05:46:07 -0600

    package updates

---
+++ project-1/src/Slides.jsx
@@ -0,0 +1,40 @@
+// Globals
+import React from "react"; // component building
+import ReactDom from "react-dom"; // rendering
+import "./App.scss";
+// Compontent
+const Root = document.getElementById("root");
+// image, headline, kicker, body
+// image - 1920, 1366, 1280, 1020, 768
+// body - richtext
+const contents = [
+  { id: 1, name: "intro", title: "intro", image: [] },
+  { id: 2, name: "brands", title: "brands", image: [] },
+  { id: 3, name: "fifa", title: "fifa", image: [] },
+  { id: 4, name: "astellas", title: "astellas", image: [] },
+  { id: 5, name: "toms", title: "toms", image: [] },
+  { id: 6, name: "empire magazine", title: "empire magazine", image: [] },
+  { id: 7, name: "spark conference", title: "spark conference", image: [] },
+];
+// Component : Static
+function Slide(props) {
+  const { id, name, title } = props;
+  return (
+    <article id={id} className="slide">
+      <h2>{name}</h2>
+      <p>{title}</p>
+    </article>
+  );
+}
+// Component : Dynamic
+function Slides() {
+  return (
+    <section>
+      {contents.map((content) => {
+        return <Slide key={content.id} {...content}></Slide>;
+      })}
+    </section>
+  );
+}
+// Rendering
+ReactDom.render(<Slides />,Root);
--- project-1/src/index.js
+++ project-1/src/index.js
@@ -1,31 +1,44 @@
 // Globals
-import React from 'react'; // component building
-import ReactDom from 'react-dom'; // rendering
-import './App.css' // Styling
-import './Hero'
+import React from "react"; // component building
+import ReactDom from "react-dom"; // rendering
+// import { BrowserRouter } from "react-router-dom";
+import "./App.scss"; // S
 // Compontent
-const Root = document.getElementById('root');
-const fullname = 'Isaiah Monroe Davis';
-const title = 'Web Designer';
-const profileImage = '#';
-// Component
-// React componenets are detected by sentence casing
-// Always return html 
-
-
-function Greeting(props) {
-  console.log(props);
+const Root = document.getElementById("root");
+// image, headline, kicker, body
+// image - 1920, 1366, 1280, 1020, 768
+// body - richtext
+const contents = [
+  { id: 1, name: "intro", title: "intro", image: [] },
+  { id: 2, name: "brands", title: "brands", image: [] },
+  { id: 3, name: "fifa", title: "fifa", image: [] },
+  { id: 4, name: "astellas", title: "astellas", image: [] },
+  { id: 5, name: "toms", title: "toms", image: [] },
+  { id: 6, name: "empire magazine", title: "empire magazine", image: [] },
+  { id: 7, name: "spark conference", title: "spark conference", image: [] },
+];
+// Component : Static
+function Slide(props) {
+  const { id, name, title } = props;
   return (
-    <div id="wrapper" className='wrapper'>
-      <h1 className='title' style={{ color: 'red' }}>My name is {fullname}</h1>
-      <h3>
-        {title}
-      </h3>
-      <img src={profileImage} alt={fullname}/>
-    </div>
+    <article id={id} className="slide">
+      <h2>{name}</h2>
+      <p>{title}</p>
+    </article>
+  );
+}
+
+// Component : Dynamic
+function Slides() {
+  return (
+    <section>
+      {contents.map((content) => {
+        return <Slide key={content.id} {...content}></Slide>;
+      })}
+    </section>
   );
 }
 
 // Rendering
 // components being rendered must have a closing tag
-ReactDom.render(<Greeting job='developer' />, Root);
+ReactDom.render(<Slides />,Root);
